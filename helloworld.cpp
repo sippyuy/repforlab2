103bb46bf37554bbff2ae99421f31eb051fff286
sippyuy <gbortelis@mail.ru>
2023-05-16 22:19:37 +0300

    enhanced helloworld.cpp

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main(){
 cout << "Hello, world!";
+string name;
+cout << "Enter your name";
+cin << name;
+cout << "Helloworld from " << name;
 return 0;
 }
